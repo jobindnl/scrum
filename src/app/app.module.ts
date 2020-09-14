4c101d4290df222c11159fa2b01912284479f2bc
Jobin Daniel <jobindnl@gmail.com>
2020-09-13 20:19:28 -0400

    changed error on directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LandingPageComponent } from './app/landing-page/landing-page.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
